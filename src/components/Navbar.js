8118866815c1e7b6348f7e9a08205cf5e3a657e3
Joshua Scott <jscott313@gmail.com>
2018-07-27 15:34:34 +0300

    Navbar adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,66 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+import carrot from '../assets/carrot.png'
+import hamburger from '../assets/hamburger.svg'
+import closeIcon from '../assets/close-icon.svg'
+
+const NavWrapper = styled.nav`
+  padding-top: 1.5rem;
+  display: flex;
+  justify-content: center;
+`
+
+const MenuToggleBtn = styled.button`
+  position: absolute;
+  left: 0;
+  top: 2.5rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`
+
+const MenuToggleImg = styled.img``
+
+const CarrotLink = styled(Link)``
+
+const CarrotImg = styled.img`
+  height: 6rem;
+  width: auto;
+`
 
 export default class Navbar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  toggleMenu = () => {
+    this.setState(
+      prevState => ({
+        isOpen: !prevState.isOpen,
+      }),
+      () => {
+        console.log(
+          `You just ${this.state.isOpen ? 'opened' : 'closed'} the navbar!`
+        )
+      }
+    )
+  }
+
   render() {
     return (
-      <div>
-        <div>Navbar</div>
-      </div>
+      <NavWrapper>
+        <MenuToggleBtn onClick={this.toggleMenu}>
+          <MenuToggleImg
+            src={this.state.isOpen ? closeIcon : hamburger}
+            alt="Toggle menu icon"
+          />
+        </MenuToggleBtn>
+        <CarrotLink to="/" title="Home">
+          <CarrotImg src={carrot} alt="Wunder logo" />
+        </CarrotLink>
+      </NavWrapper>
     )
   }
 }
