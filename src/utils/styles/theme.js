eaba022b547df923d6ea7031906f9d317cd0ccd4
Joshua Scott <jscott313@gmail.com>
2018-07-30 18:29:58 +0300

    Minor theme adjustments

--- src/utils/styles/theme.js
+++ src/utils/styles/theme.js
@@ -38,7 +38,7 @@ injectGlobal`
   }
 
   h1 {
-    font-size: 5.9rem;
+    font-size: 4.5rem;
   }
   h2 {
     font-size: 4rem;
@@ -72,28 +72,28 @@ const theme = {
     large: `all and (min-width: ${1200 / 16}em)`, // 1200px
   },
   colours: {
-    grey: '#EFF3F5',
+    grey: '#eff3f5',
     greySecondary: '#666666',
-    greyDarker: '#C9D1D1',
-    grayLight: '#E6ECEE',
+    greyDarker: '#c9d1d1',
+    grayLight: '#e6ecee',
     mystic: '#f1f5f8',
-    red: '#E20B61',
+    red: '#e20b61',
     darkHotPink: '#e50061',
-    lipstick: '#B4004C',
-    redDark: '#71003A',
-    purple: '#350C4D',
-    purpleDark: '#1D082D',
-    cyan: '#23BCD9',
-    cyanLight: '#00D5F7',
-    orange: '#FD972B',
-    orange2: '#FF9600',
+    lipstick: '#b4004c',
+    redDark: '#71003a',
+    purple: '#350c4d',
+    purpleDark: '#1d082d',
+    cyan: '#23bcd9',
+    cyanLight: '#00d5f7',
+    orange: '#fd972b',
+    orange2: '#ff9600',
     navy: '#021b35',
-    white: '#FFFFFF',
-    pizazz: '#FF9600',
-    robinsEggBlue: '#00BDDB',
+    white: '#ffffff',
+    pizazz: '#ff9600',
+    robinsEggBlue: '#00bddb',
     backgroundBlackenedBlue: '#02172c',
     backgroundDarkPurple: '#1d082c',
-    purpleLight: '#CF93F9',
+    purpleLight: '#cf93f9',
     purpleDarker: '#7200cc',
     textGray: '#666666',
     green: '#00ae9f',
