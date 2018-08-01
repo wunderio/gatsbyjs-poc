dfb39be84fa395549128999fb6bbdd40e30efda3
Joshua Scott <jscott313@gmail.com>
2018-08-01 16:50:10 +0300

    Add styling for blog posts

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,22 +1,80 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 
-export default ({ data }) => {
-  const post = data.markdownRemark
+const BlogText = styled.div`
+  font-size: 1.8rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  margin-top: 2rem;
+`
+
+const PostTitle = styled.h1`
+  font-size: 3.95rem;
+  font-weight: 800;
+  color: ${({ theme }) => theme.colours.cyan};
+  text-align: start;
+  text-transform: uppercase;
+
+  @media ${({ theme }) => theme.breakpoints.small} {
+    font-size: 5.9rem;
+  }
+`
+const Date = styled.div`
+  color: ${({ theme }) => theme.colours.textGray};
+  font-weight: 300;
+`
+
+const Content = styled.article`
+  h2,
+  h3 {
+    margin: 4.3rem 0 2.4rem;
+    line-height: 1.3;
+    letter-spacing: 0.2rem;
+    text-align: start;
+  }
+
+  h2 {
+    font-size: 3.96rem;
+  }
+
+  h3 {
+    font-size: 2.52rem;
+  }
+
+  li {
+    margin: 2rem 0;
+  }
+
+  p {
+    font-size: 1.8rem;
+    font-weight: 300;
+  }
+`
+
+const BlogPost = props => {
+  const post = props.data.markdownRemark
+
   return (
-    <Layout
-      pathname={post.fields.slug}
-      title="Blog post"
-      colourScheme="standard"
-    >
-      <h2>{post.frontmatter.title}</h2>
-      <p>
-        By <strong>{post.frontmatter.author}</strong>
-      </p>
-      <article dangerouslySetInnerHTML={{ __html: post.html }} />
+    <Layout colourScheme="standard">
+      <BlogText>Blog</BlogText>
+      <PostTitle>{post.frontmatter.title}</PostTitle>
+      <Date>{post.frontmatter.date}</Date>
+      {/* <AuthorInfo>
+        <AuthorImage>{'todo'}</AuthorImage>
+        <AuthorName>{post.author || 'A. Wunderer'}</AuthorName>
+        <AuthorPosition>{post.author.position || 'Wunderer'}</AuthorPosition>
+      </AuthorInfo> */}
+      <Content dangerouslySetInnerHTML={{ __html: post.html }} />
     </Layout>
   )
+}
+
+BlogPost.propTypes = {
+  // todo: Define the shape properly
+  data: PropTypes.object.isRequired,
 }
 
 export const query = graphql`
@@ -26,6 +84,7 @@ export const query = graphql`
       frontmatter {
         title
         author
+        date(formatString: "DD MMMM YYYY")
       }
       fields {
         slug
@@ -33,3 +92,5 @@ export const query = graphql`
     }
   }
 `
+
+export default BlogPost
