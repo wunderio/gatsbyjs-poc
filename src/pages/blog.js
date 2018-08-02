699778924d57d78c7f113c513cf9d5567cb77b40
Joshua Scott <jscott313@gmail.com>
2018-08-02 17:09:13 +0300

    Move Layout.js from components/ to templates/ Since it defines the structure of a whole page, it's better suited here.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
-import Layout from 'components/Layout'
+import Layout from 'templates/Layout'
 import BlogListItem from 'components/BlogListItem'
 
 const BlogList = styled.ul`
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { graphql } from 'gatsby'
-import Layout from 'components/Layout'
+import Layout from 'templates/Layout'
 
 const BlogText = styled.div`
   font-size: 1.8rem;
